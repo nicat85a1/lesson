304e73cdee8c69af8fa076e6d7b4fd2160b36efb
Nicat <nicat85a1@gmail.com>
2024-03-02 22:19:14 +0400

    ram+ssd+pil_durumu

--- pil_sağlığı_öğrenme.py
+++ pil_sağlığı_öğrenme.py
@@ -1,4 +1,39 @@
 import os
+import re
+import psutil
+import shutil
+
+# Bölümlerin isimlerini bir liste olarak tanımla
+partitions = ["C:\\", "D:\\", "E:\\"]
+
+# Toplam değerleri sıfırla
+total_ssd = 0
+used_ssd = 0
+free_ssd = 0
+
+# Her bir bölüm için disk kullanımını al ve toplama ekle
+for partition in partitions:
+    total, used, free = shutil.disk_usage(partition)
+    total_ssd += total
+    used_ssd += used
+    free_ssd += free
+
+print()
+print("SSD Kullanımı:")
+
+# Toplam değerleri GB cinsinden yazdır
+print("Toplam SSD:", total_ssd // (2**30), "GB")
+print("Kullanılan SSD:", used_ssd // (2**30), "GB")
+print("Boş SSD:", free_ssd // (2**30), "GB")
+print("SSD Kullanım Yüzdesi:", used_ssd / total_ssd * 100, "%")
+
+# Sistem belleği kullanımı hakkında bir isimlendirilmiş demet döndür
+memory = psutil.virtual_memory()
+print()
+print("Bellek Kullanımı:")
+
+# Toplam RAM, kullanılabilir RAM, kullanılan RAM ve RAM kullanım yüzdesini yazdır
+print("RAM Kullanım Yüzdesi:", memory.percent,"%")
 
 # powercfg'nin tam yolunu belirtin (genellikle C:/Windows/System32/)
 powercfg_path = 'C:/Windows/System32/'
@@ -9,10 +44,7 @@ os.environ['PATH'] = f"{powercfg_path};{os.environ['PATH']}"
 # powercfg /batteryreport komutunu çalıştırın
 os.system('powercfg /batteryreport')
 
-# re modülü
-import re
-
-dosya_adi = "C:/Users/user/battery-report.html"
+dosya_adi = "C:/Users/user/Desktop/python/battery-report.html"
 
 # Kapasite kelimesini arayalım
 aranan_kelime = "CAPACITY"
@@ -47,6 +79,9 @@ for satir in dosya:
                 break
 # Dosyayı kapıyoruz
 dosya.close()
+
+print()
+print("pil durumu:")
 
 # Sayıların listesini yazdırıyoruz
 print(f"kapasite: {sayilar}")
