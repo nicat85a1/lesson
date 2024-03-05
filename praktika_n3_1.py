965fc01fb553157b83f2e4b52e5bf7dc8d92279b
Nicat <nicat85a1@gmail.com>
2024-03-05 17:10:52 +0400

    praktika

---
+++ praktika_n3_1.py
@@ -0,0 +1,66 @@
+"""
+a = (1,2,3,4,5,6,7,8,9,10)
+b = list(a)
+print(b)
+
+"""
+"""
+a = (1,2,3,4,5,6,7,8,9,10)
+b = a[0:]
+print(b)
+"""
+"""
+a = [1,2,3,4,5,6,7,8,9,10]
+print(a.reverse())
+"""
+"""
+testlist = [1,2,3,4,5,6,7,8,9,10]
+
+testlist[2] = 2
+
+print(testlist)
+
+"""
+"""
+testlist = [1,2,3,4,5,6,7,8,9,10]
+testlist.pop(1)
+testlist.pop(2)
+testlist.pop(3)
+testlist.pop(4)
+testlist.pop(5)
+print(testlist)
+"""
+"""
+test_list = [6, 7, [], 3, [], [], 9]
+
+test_list.pop(2)
+test_list.pop(3)
+test_list.pop(3)
+print(test_list)
+
+test_list = [6, 7, [], 3, [], [], 9]
+
+test_list.pop(2)
+test_list.pop(4)
+test_list.pop(3)
+print(test_list)
+"""
+"""
+
+test_list = [1, 6, 7, 3, 9, 100, 200]
+
+test_list.sort()
+print(test_list)
+"""
+"""
+test_list = [1, 6, 7, 3, 9, 100, 200]
+
+test_list.reverse()
+print(test_list)
+"""
+"""
+test_list = (1, 6, 7, 3, 9, 100, 200)
+test_list1 = (3,4)
+test_list += test_list1
+print(test_list)
+"""
