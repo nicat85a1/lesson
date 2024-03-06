7e4b9a06338eb5e8cea6a46f5186dd45ac9541a8
Nicat <nicat85a1@gmail.com>
2024-03-06 17:45:37 +0400

    main

---
+++ praktika_n4.py
@@ -0,0 +1,140 @@
+"""
+# task 1 +
+
+listcem= [2, 3, 4, 5]
+toplam=0
+for sayi in listcem:
+   toplam+=sayi
+print(toplam)
+
+hasil=1
+for sayi in listcem:
+   hasil*=sayi
+print(hasil)
+
+# task 2 +
+
+listsira = [2,4,5,9]
+listsira.reverse()
+print(listsira)
+"""
+"""
+# task 3 +
+list1 = ["sa","sa","sa"]
+list2 = ["lam","lam","lam"]
+listzip = zip(list1,list2)
+bosluk = tuple((a + b,) for a, b in listzip)
+print(bosluk)
+"""
+
+# task 4 +
+
+a = [5,7,["aplle","gilas","heyva"]]
+a[2][2]="gül"
+print(a)
+
+
+"""
+# task 5 +
+
+listindexx = [1,2,3,4,5]
+listindexx[2]=2
+print(listindexx)
+
+# tas 6
+
+listorta = [1,2,3,4,5,6,7,8,9,10]
+toplam=0
+for sayi in listorta:
+   toplam+=sayi
+print(toplam/len(listorta))
+"""
+"""
+# task 7
+
+reqem1 = int(input("reqem1: "))
+reqem2 = int(input("reqem2: "))
+
+cüt = []
+tək = []
+
+for reqem in range(reqem1, reqem2 + 1):
+    if reqem % 2 == 0:
+        cüt.append(reqem)
+    else:
+        tək.append(reqem)
+
+print(f"Cüt: {cüt}")
+print(f"Tək: {tək}")
+print(f"Cüt rəqəmlərin sayı: {len(cüt)}")
+
+"""
+"""
+# task 8 +
+
+listtekrar = [1,2,2,4,4,6,6,8,8,10]
+print(set(listtekrar))
+
+"""
+# task 9
+
+import itertools
+
+reqemler = [1, 2, 3]
+
+kombinasiyalar = list(itertools.permutations(reqemler))
+for komb in kombinasiyalar:
+    print(komb)
+
+
+"""
+# task 10
+a = {'a':['Fuad','Ali','Orxan']}
+secim = input("(1) Elave et (2) Sil:")
+if secim == "1":
+    ad = input("Ad:")
+    if ad == "":
+        print("Ad bos ola bilmez")
+    elif ad in a["ad"]:
+        print("Bu ad artiq movcuddur")
+    else:
+        a["ad"].append(ad)
+        print("Ad elave olundu")
+elif secim == "2":
+    ad = input("Ad:")
+    if ad == "":
+        print("Ad bos ola bilmez")
+    elif ad in a["ad"]:
+        print("Bu ad movcuddur")
+    else:
+        a["ad"].remove(ad)
+        print("Ad silindi")
+elif secim == "2":
+    ad = input("Ad:")
+    if ad == "":
+        print("Ad bos ola bilmez")
+    elif ad in a["ad"]:
+        print("Bu ad artiq movcuddur")
+    else:
+        a["ad"].remove(ad)
+        print("Ad elave olundu")
+elif secim == "2":
+    ad = input("Ad:")
+    if ad == "":
+        print("Ad bos ola bilmez")
+    elif ad in a["ad"]:
+        print("Bu ad movcuddur")
+    else:
+        a["ad"].append(ad)
+        print("Ad silindi")
+"""
+
+"""
+# task 11
+
+i = 0
+
+while i < 10 :
+    print("i",i) # döngüler
+    i = i + 2 # i+=2 # i*=2 ve s. da kullanabilirsin
+"""
