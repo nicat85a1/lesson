df6e449c23733177133e8915fa0507aa20079320
Nicat <nicat85a1@gmail.com>
2024-03-12 20:40:58 +0400

    import re update

--- Classroom_n2.py
+++ Classroom_n2.py
@@ -143,10 +143,15 @@ for i in range(3):
     print(rakamlar)
     """
 
-# for in for oto yapabilirmiyim?
+# Recursion
 
-rakamlar = [1, 2, 3]
-for saygac1 in rakamlar:
-    for saygac2 in rakamlar:
-        for saygac3 in rakamlar:
-            print([saygac1, saygac2, saygac3])
+rakamlar = [1, 2, 3, 4]
+
+def forinfor(döngü, mevcut_döngü=0, mevcut_sonuç=[]):
+    if mevcut_döngü < döngü:
+        for saygac in rakamlar:
+            forinfor(döngü, mevcut_döngü + 1, mevcut_sonuç + [saygac])
+    else:
+        print(mevcut_sonuç)
+
+forinfor(len(rakamlar))
---
+++ ders.py
@@ -0,0 +1,43 @@
+# task 1
+"""
+kütle = float(input("çeki daxil et: "))
+boy = float(input("boy daxil et: "))
+
+BKI = kütle / ((boy / 100)**2)
+
+print("BKI:", BKI)
+
+if BKI < 18.5:
+    print("zeif")
+elif BKI > 18.5 and BKI < 25:
+    print("normal")
+elif BKI > 25 and BKI < 30:
+    print("kilolu")
+elif BKI > 30:
+    print("obez")
+    """
+
+# task 2
+
+şifre = input("şifrenizi daxil edin: ")
+if len(şifre) < 6:
+    print("şifre 6 simvoldan azdir")
+elif len(şifre) > 16:
+    print("şifre 16 simvoldan çoxdur")
+
+import re
+
+x = re.findall("[a-z]", şifre)
+z = re.findall("[A-Z]", şifre)
+v = re.findall("[$#@]", şifre)
+b = re.findall("[0-9]", şifre)
+print(x, z, v, b)
+
+if not (x):
+    print("a-z yoxdur")
+if not (z):
+    print("A-Z yoxdur")
+if not (v):
+    print("simvol yoxdur")
+if not (b):
+    print("0-9 yoxdur")
