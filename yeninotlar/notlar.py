a54da6c53ec175ea3854608af73ad1989e717b98
Nicat <nicat85a1@gmail.com>
2024-03-17 14:51:30 +0400

    update

---
+++ yeninotlar/notlar.py
@@ -0,0 +1,43 @@
+# print(*range(10))
+"""
+# hatalı kod
+
+i = 0
+
+while i < 10:
+    if i == 2:
+        continue
+    i+=1
+    print(i) # sonsuz döngü
+"""
+"""
+def topla(liste):
+      if (len(liste)) == 0:
+            return 0
+      else:
+            return liste[0] + (topla(liste[1:]))
+ # 1. return 1 + topla (2,3,4)
+ # 2. return 2 + topla (3,4)
+ # 3. return 3 + topla (4)
+ # 4. return 4 + topla ()
+ # 5. return 0
+ # 1 + (2 +(3 + (4 + ())
+
+print(topla([1,2,3,4]))
+"""
+"""
+a=10
+def harf():
+    global a # globaldaki veriyi değiştir
+    a=2
+    print(a)
+harf()
+print(a)
+"""
+"""
+def harf():
+    a=2
+    b=5
+    return a # return b
+print(harf())
+"""
