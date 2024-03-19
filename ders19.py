0245394f806ab3f57883f2e99df97d5315de500d
Nicat <nicat85a1@gmail.com>
2024-03-19 15:21:21 +0400

    update

---
+++ ders19.py
@@ -0,0 +1,42 @@
+"""
+
+def num(x):
+    if x == -1:
+        return "hello"
+    return num(x-1)
+num(5)
+print(num(5))
+
+num = lambda x: x if x == -1 else num(x-1)
+"""
+"""
+def tax_calculator(price, quantity):
+    return price * quantity * 1.18
+
+def product(name,price,quantity):
+    tax = tax_calculator(price,quantity)
+
+    product_dict = {
+        "name":name,
+        "price":price,
+        "quantity":quantity,
+        "tax":tax
+    }
+    return product_dict
+
+print(product("apple",10,5))
+"""
+"""
+def product(name,price,quantity):
+    tax = lambda price,quantity: price * quantity * 1.18
+
+    product_dict = {
+        "name":name,
+        "price":price,
+        "quantity":quantity,
+        "tax":tax(price,quantity)
+    }
+    return product_dict
+
+print(product("apple",10,5))
+"""
