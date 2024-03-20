63566a8fd655c1dc01ca8b18b5a41a33741652a9
Nicat <nicat85a1@gmail.com>
2024-03-21 01:11:57 +0400

    error message

--- evaluation.py
+++ evaluation.py
@@ -1,7 +1,4 @@
 def student_grades(students, subjects):
-    if not isinstance(students, (list, dict, tuple)) and isinstance(subjects, (list, dict, tuple)):
-        print("The 'students' parameter must be a list, dictionary, or tuple.")
-        return
     grades = {student: [] for student in students}
     for student in students:
         for subject in subjects:
