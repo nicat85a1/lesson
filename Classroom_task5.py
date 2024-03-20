0a43f656dfd4f93ec26de453240ba3672d224ba4
Nicat <nicat85a1@gmail.com>
2024-03-20 21:03:15 +0400

    update

--- Classroom_task5.py
+++ Classroom_task5.py
@@ -8,15 +8,15 @@ from students_budget import budget
 students = ["Albert","Arthur"]
 
 budget(students)
-
+"""
 # Səhiyyə Sənayesi:
 
 from bmi import bmi_calculator
 
 result,status = bmi_calculator()
 print(f"BMI: {result} , Status: {status}.")
-"""
 
+"""
 # Təhsil Sənayesi:
 
 from evaluation import student_evaluation
@@ -25,3 +25,4 @@ students = ["Albert", "Arthur"]
 subjects = ["English", "informatics", "math", "python"]
 
 student_evaluation(students, subjects)
+"""
--- bmi.py
+++ bmi.py
@@ -1,6 +1,15 @@
-def bmi_calculator(*args):
-    weight = float(input("Enter your weight (kg): "))
-    height = float(input("Enter your height (meters): "))
+def bmi_calculator():
+    while True:
+        try:
+            weight = float(input("Enter your weight (kg): "))
+            height = float(input("Enter your height (meters): "))
+            if 0 <= weight <= 727 and 0 <= height <= 2.51:
+                break
+            else:
+                print("Please enter a weight between 0 and 727")
+                print("Please enter a height between 0 and 2.51")
+        except ValueError:
+            print("Please enter a valid number.")
     bmi = weight / (height ** 2)
     if bmi < 18.5:
         return f"{bmi:.2f}","Underweight"
