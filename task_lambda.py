d34c5ce6ba4ba0c2ead842d9d0ee1a0b46250f36
Nicat <nicat85a1@gmail.com>
2024-03-27 17:50:50 +0400

    update

--- task_lambda.py
+++ task_lambda.py
@@ -5,12 +5,12 @@ if num ** 0.5 % 1 == 0:
 else:
     print("The number is not a perfect square.")
 """
-"""
+
 # task 1 lambda
 square_root = lambda num: f"The square root of the number is: {num**0.5:.0f}" if num ** 0.5 % 1 == 0 else "The number is not a perfect square."
 num = int(input("Enter a number: "))
 print(square_root(num))
-"""
+
 
 """
 def exponent_function(nums):
