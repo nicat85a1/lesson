8d1616ba3132322c726d79fce020d7b64f9899d9
Nicat <nicat85a1@gmail.com>
2024-04-13 15:01:57 +0400

    UPDATE

--- classtask2.py
+++ classtask2.py
@@ -25,22 +25,17 @@
 print(py_solution().int_to_Roman(2))
 print(py_solution().int_to_Roman(4000))"""
 
-"""class py_solution:
+class py_solution:
     def roman_to_int(self, s):
         rom_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         int_val = 0
         for i in range(len(s)):
-            if i > 0 and rom_val[s[i]] > rom_val[s[i - 1]]:
-                int_val += rom_val[s[i]] - 2 * rom_val[s[i - 1]]
-                print("int_val",int_val)
-                print("i",i)
-                print("rom_val[s[i]] > rom_val[s[i - 1]]",rom_val[s[i]] > rom_val[s[i - 1]],rom_val[s[i]], rom_val[s[i - 1]])
-                print("int_val += rom_val[s[i]] - 2 * rom_val[s[i - 1]]",int_val, rom_val[s[i]] - 2 * rom_val[s[i - 1]],rom_val[s[i]], 2 * rom_val[s[i - 1]])
+            if i > 0 and rom_val[s[i]] > rom_val[s[i - 1]]: # 1.ci index 0.cı index den böyükdürsə
+                int_val += rom_val[s[i]] - 2 * rom_val[s[i - 1]] # 2 * menası: (int_val += rom_val[s[i]] - rom_val[s[i - 1]])   (5-1=4)   (int_val += rom_val[s[i]]) (4+1=5)   hesablama sehf olur.
             else:
                 int_val += rom_val[s[i]]
-                print("int_val += rom_val[s[i]]",int_val, rom_val[s[i]])
         return int_val
 
-print("XL",py_solution().roman_to_int('XL'))
-print("L",py_solution().roman_to_int('L'))
-print("LX",py_solution().roman_to_int('LX'))"""
+print(py_solution().roman_to_int('IV')) # index 0,1
+print(py_solution().roman_to_int('V')) # index 0
+print(py_solution().roman_to_int('VII')) # index 0,1,2
