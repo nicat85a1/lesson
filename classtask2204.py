d1432f0d689394888e7659b2eb586c245d9c7bfb
Nicat <nicat85a1@gmail.com>
2024-04-22 02:04:28 +0400

    update

--- classtask2204.py
+++ classtask2204.py
@@ -94,3 +94,49 @@ School_bus = Bus("School Volvo", 12, 50)
 
 # Python's built-in isinstance() function
 print(isinstance(School_bus, Vehicle))"""
+
+"""import random
+
+class flashcard:
+	def __init__(self):
+	
+		self.fruits={'apple':'red',
+					'orange':'orange',
+					'watermelon':'green',
+					'banana':'yellow'}
+		
+	def quiz(self):
+		while (True):
+		
+			fruit, color = random.choice(list(self.fruits.items()))
+			
+			print("What is the color of {}".format(fruit))
+			user_answer = input()
+			
+			if(user_answer.lower() == color):
+				print("Correct answer")
+			else:
+				print("Wrong answer")
+				
+			option = int(input("enter 0 , if you want to play again : "))
+			if (option):
+				break
+
+print("welcome to fruit quiz ")
+fc=flashcard()
+fc.quiz()"""
+
+if 0 == True:
+    print("True")
+else:
+    print("False")
+
+if 1 == True:
+    print("True")
+else:
+    print("False")
+
+if -1 == True:
+    print("True")
+else:
+    print("False")
