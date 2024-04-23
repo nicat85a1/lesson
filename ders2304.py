d0a9a6395510ed9544a950837769496002e27220
Nicat <nicat85a1@gmail.com>
2024-04-23 23:15:01 +0400

    update

--- ders2304.py
+++ ders2304.py
@@ -6,15 +6,19 @@ class Person:
         self.wheel = Person.__wheel
 
     def person_info(self):
-        return f"Name: {self.firstname} {self.lastname}, Wheels: {self.wheel}"
+        return f"Name: {self.firstname} {self.lastname}, Wheels: {Person.__wheel}" # Wheels: {self.__wheel}
 
 Person_ = Person("John", "Doe")
 
 class Student(Person):
-    pass
+    def student_info(self,wheel=Person_.wheel):
+        self.wheel = wheel
+        return f"Name: {self.firstname} {self.lastname}, Wheels: {self.wheel}"
 
 Student_ = Student("Mike", Person_.lastname)
 
 print(Person_.person_info())
 
 print(Student_.person_info())
+
+print(Student_.student_info())
