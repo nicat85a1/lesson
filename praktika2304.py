7101bd956e43eebb2d379b461d7dd171a811139b
Nicat <nicat85a1@gmail.com>
2024-04-23 21:39:24 +0400

    update

--- praktika2304.py
+++ praktika2304.py
@@ -8,8 +8,10 @@ try:
     file.close()
 except ZeroDivisionError:
     print("0 dan yuxarı reqem girin")"""
+
 # task2
-"""file = open("C:/Users/user/Desktop/praktika2.txt","w")
+
+file = open("C:/Users/user/Desktop/praktika2.txt","w")
 file.write("10 11 12 13 14 15 16 17 18 19 20")
 file = open("C:/Users/user/Desktop/praktika2.txt","r")
 x = file.read()
@@ -19,11 +21,64 @@ for i in x:
         file2 = open("C:/Users/user/Desktop/praktika3.txt","a+")
         file2.write(str(i)+" ")
         file2.close()
-    try:
-        cavab = int(i) / 0
-    except ZeroDivisionError:
-        print("0 dan yuxarı reqemler yazilmadi")"""
-
+    elif int(i) % 2 != 0:
+        try:
+            cavab = int(i) / 0
+        except ZeroDivisionError:
+            print("0 dan yuxarı reqemler yazilmadi")
 
 # task3
 
+"""Məhsullar = {
+    "iphone": "1400",
+    "samsung": "400",
+    "xiaomi": "200"
+}
+
+with open("C:/Users/user/Desktop/praktika4.txt", "w") as file:
+    for key, value in Məhsullar.items():
+        file.write(f"{key}: {value}\n")
+file = open("C:/Users/user/Desktop/praktika4.txt","r")
+x = file.read()
+x = x.split()
+total = 0
+for _ in range(3):
+    choise_shop = input("Məhsulun adini daxil edin: ")+":"
+    if choise_shop in x:
+        if choise_shop == "iphone:":
+            print(f"Məhsulun qiymeti: {x[1]}")
+            total += int(x[1])
+            print(total)
+            try:
+                if total >= 1500:
+                    print(f"Sizin toplam məbləğiniz: {total}")
+                    if total / 0 == 0:
+                        print()
+            except:
+                print("icazə verilən məbləği keçdiniz, yenidən seçəm edin və ya sifarişi sonlandırın")
+                total -= int(x[1])
+        elif choise_shop == "samsung:":
+            print(f"Məhsulun qiymeti: {x[3]}")
+            total += int(x[3])
+            print(total)
+            try:
+                if total >= 1500:
+                    print(f"Sizin toplam məbləğiniz: {total}")
+                    if total / 0 == 0:
+                        print()
+            except:
+                print("icazə verilən məbləği keçdiniz, yenidən seçəm edin və ya sifarişi sonlandırın")
+                total -= int(x[1])
+        elif choise_shop == "xiaomi:":
+            print(f"Məhsulun qiymeti: {x[5]}")
+            total += int(x[5])
+            print(total)
+            try:
+                if total >= 1500:
+                    print(f"Sizin toplam məbləğiniz: {total}")
+                    if total / 0 == 0:
+                        print()
+            except:
+                print("icazə verilən məbləği keçdiniz, yenidən seçəm edin və ya sifarişi sonlandırın")
+                total -= int(x[1])
+print("Yekun ödəniş: ",total)"""
